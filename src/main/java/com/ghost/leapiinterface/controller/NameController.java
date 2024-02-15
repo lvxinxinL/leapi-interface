4dc39ce54ec33292b9e839c4ea9c8f991e81bbc6
Ghost <2089049079@qq.com>
2024-02-15 14:59:29 +0800

    add 修改输出日志级别为 trace

--- src/main/java/com/ghost/leapiinterface/controller/NameController.java
+++ src/main/java/com/ghost/leapiinterface/controller/NameController.java
@@ -1,9 +1,7 @@
 package com.ghost.leapiinterface.controller;
 
 import com.ghost.leapiclientsdk.model.User;
-import com.ghost.leapiclientsdk.utils.SignUtil;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -16,12 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/name")
 public class NameController {
 
-    @GetMapping
+    @GetMapping("/get")
     public String getNameByGet(String name) {
         return "GET 你的名字是：" + name;
     }
 
-    @PostMapping("/")
+    @PostMapping("/post")
     public String getNameByPost(@RequestParam String name) {
         return "POST 你的名字是：" + name;
     }
@@ -29,27 +27,26 @@ public class NameController {
     @PostMapping("/user")
     public String getNameByJSON(@RequestBody User user, HttpServletRequest request) {
         // 获取请求头中携带的参数，校验调用接口的权限
-        String accessKey = request.getHeader("accessKey");
+//        String accessKey = request.getHeader("accessKey");
 //        String secretKey = request.getHeader("secretKey");
-        String nonce = request.getHeader("nonce");
-        String body = request.getHeader("body");
-        String timestamp = request.getHeader("timestamp");
-        String sign = request.getHeader("sign");
+//        String nonce = request.getHeader("nonce");
+//        String body = request.getHeader("body");
+//        String timestamp = request.getHeader("timestamp");
+//        String sign = request.getHeader("sign");
         // TODO 实际上要从数据库查是否已分配给用户
-        if (!accessKey.equals("ghost")) {
-            throw new RuntimeException("无权限");
-        }
+//        if (!accessKey.equals("ghost")) {
+//            throw new RuntimeException("无权限");
+//        }
         // 校验随机数
-        if (nonce.length() > 5) {
-            throw new RuntimeException("无权限");
-        }
+//        if (nonce.length() > 5) {
+//            throw new RuntimeException("无权限");
+//        }
 
-        // TODO 校验时间戳 timestamp：和当前时间不能超过 5 min
         // 和客户端使用同一套加密算法进行校验
-        String serverSign = SignUtil.genSign(body, "abcdefg");// 实际上要从数据库中取出数据进行校验
-        if (!serverSign.equals(sign)) {
-            throw new RuntimeException("无权限");
-        }
+//        String serverSign = SignUtil.genSign(body, "abcdefg");// 实际上要从数据库中取出数据进行校验
+//        if (!serverSign.equals(sign)) {
+//            throw new RuntimeException("无权限");
+//        }
 //        System.out.println("getNameByJSON 被调用，accessKey: " + accessKey + "; secretKey:" +secretKey);
         return "POST 你的名字是：" + user.getUsername();
     }
